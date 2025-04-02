6158811d2c81ac2fb8e36ffa664dca92c990914c
Krittaphat01 <119992232+Krittaphat01@users.noreply.github.com>
2025-04-02 21:02:25 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5133, // กำหนดพอร์ต (ไม่จำเป็นต้องเปลี่ยนถ้าใช้ค่าดีฟอลต์)
+    open: true, // เปิดเบราว์เซอร์อัตโนมัติเมื่อรัน dev server
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // ป้องกัน Vite แยกโค้ดออกเป็น chunks ที่อาจทำให้ routing พัง
+      },
+    },
+  },
+  esbuild: {
+    
+  },
+});
